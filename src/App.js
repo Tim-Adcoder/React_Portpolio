d72dc6287e77af629f8e27c6d5a570d23eaf5dff
TimS <92215345+Tim-Adcoder@users.noreply.github.com>
2022-08-05 17:17:51 +0930

    final push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./Home";
+import About from "./About"
+import Project from "./Project"
+import Contact from "./Contact"
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Project />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
